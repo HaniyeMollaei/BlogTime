611c49dc679cdbf610caccb266333d4d90be0b43
Haniye Mollaei <HaniyeMolaei1378@gmail.com>
2020-11-19 22:03:23 +0330

    complete ;og in , profile , feed

---
+++ app/src/main/java/com/example/blog/activity/Feed.kt
@@ -0,0 +1,4 @@
+package com.example.blog.activity
+
+class Feed {
+}
--- app/src/main/java/com/example/blog/dataclass/Const.kt
+++
@@ -1,7 +0,0 @@
-package com.example.blog.dataclass
-
-class Const{
-    companion object {
-        final val BASE_URL = "https://jsonplaceholder.typicode.com/"
-    }
-}
